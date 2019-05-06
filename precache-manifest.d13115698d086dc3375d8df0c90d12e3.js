6e4e13189b20248120b4c1aa965e09fd91172108
root <bethancourt@alice.it>
2019-05-06 11:38:39 -0500

    Se suben los archivos

---
+++ precache-manifest.d13115698d086dc3375d8df0c90d12e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "674c66a6235e7ad6274e976f075d3e0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fddf153187bee9e28d6",
+    "url": "/static/css/main.b4fc6cee.chunk.css"
+  },
+  {
+    "revision": "4dc9b05c159e1bcc6239",
+    "url": "/static/js/2.a67784a0.chunk.js"
+  },
+  {
+    "revision": "9fddf153187bee9e28d6",
+    "url": "/static/js/main.1af03763.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4d4a17ac795bbe8c467f19e79e698b4b",
+    "url": "/static/media/favicon.4d4a17ac.svg"
+  }
+]);
